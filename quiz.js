b574746e8d441ed18c5ab0c2e5c7622513da93f3
dittejessen <dittejessen12@hotmail.com>
2024-10-23 13:10:38 +0200

    quiz-questions

--- quiz.js
+++ quiz.js
@@ -22,3 +22,31 @@ function answerQuestion(questionNumber, answer) {
         showResult();
     }
 }
+
+function updateProgressBar(questionNumber) {
+    const progressPercentage = (questionNumber / totalQuestions) * 100;
+    document.getElementById('progressBar').style.width = progressPercentage + '%';
+}
+
+function showNextQuestion(questionNumber) {
+    document.getElementById(`question${currentQuestion}`).classList.remove('active');
+    document.getElementById(`question${questionNumber}`).classList.add('active');
+    currentQuestion = questionNumber;
+}
+
+function showResult() {
+    document.getElementById(`question${currentQuestion}`).classList.remove('active');
+    document.getElementById('result').style.display = 'block';
+
+    let sortedAnswers = Object.keys(answers).sort((a, b) => answers[b] - answers[a]);
+
+    // Show multiple results if all answers are different
+    if (answers[sortedAnswers[0]] === 1 && answers[sortedAnswers[1]] === 1 && answers[sortedAnswers[2]] === 1) {
+        showMultipleResults();
+    } else {
+        showSingleResult(sortedAnswers[0]);
+    }
+
+    // Set progress bar to 100% on result
+    updateProgressBar(totalQuestions);
+}
